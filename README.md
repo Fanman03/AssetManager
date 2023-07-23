a593174a8989823b6949b1622fdaccd9b2d3fbc9
Jack Pendleton <jacklrpendleton@gmail.com>
2023-07-22 22:59:51 -0500

    First public code push

---
+++ README.md
@@ -0,0 +1,53 @@
+# AssetMgr
+A simple, locally-hosted asset management platform
+
+## Setup
+AssetMgr is designed to run as a Docker container. The following docker-compose file can be used as a starting point.
+```yaml
+version: '3'
+services:
+  assetmgr:
+    image: 'fanman03/assetmgr:1.0'
+    restart: always
+    container_name: "assetmgr"
+    ports:
+      - '3002:3002'
+    environment:
+      - MONGODB_URI=mongodb+srv://your_connection_string_goes_here
+      - BASE_DOMAIN="http://example.com"
+      - APP_NAME="Asset Management"
+```
+
+## Adding Assets
+Assets are represented by MongoDB objects. The following is an example asset:
+
+```json
+{
+  "_id": "2A304D9",
+  "Status": 1,
+  "Purchase_Date": 1668664800,
+  "Type": "AP",
+  "Brand": "Ubiquiti",
+  "Model": "U6-Pro",
+  "MAC": "AA:BB:CC:DD:EE:FF",
+  "Description": "Upstairs U6 Pro",
+  "Image": "Ubiquiti\\U6Pro",
+  "2.4GHz": "2x2 WiFi 6",
+  "5Ghz": "4x4 WiFi 6",
+  "Notes": "U6 Pro installed upstairs in XYZ branch office"
+}
+```
+
+The following table lists commonly used asset properties. Properties in bold are required, while all others are optional. 
+| Property  | Purpose |
+| ------------- | ------------- |
+| **_id** | Globally unique identifier for the asset. AssetMgr does not enforce any specific schema, but serial number, MAC ID, or sequential numbers are some reasonable choices. |
+| **Status** | Number describing the status of the device. `0` = Spare, `1` = In Service, `2` = Retired, `3` = Sold, `4` = Lost, `5` = Stolen. |
+| **Brand** | Brand name of the asset. |
+| **Model** | Model of the asset. |
+| **Description** | Description of the asset. |
+| Image | Image to display in the UI of the asset, from the provided [image repository](https://github.com/Fanman03/asset-images). Must be in the format `Brand\\Device`. |
+| Notes | Extended information field about the asset, supports Markdown. |
+| Serial_Number	| Serial number of the asset. If it is a valid Dell Service Tag, this property will be displayed as a hyperlink to the Dell page with information about the specific asset. |
+
+Custom properties can be defined by simply creating a new property with any spaces replaced with underscores. In the example asset, the 2.4GHz and 5GHz fields are custom properties.
---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const fs = require("fs");
+const handlebars = require("handlebars");
+
+const { MongoClient, MongoWriteConcernError } = require("mongodb");
+const crypto = require('crypto');
+let client = "invalid";
+try {
+    const MONGODB_URI = process.env.MONGODB_URI;
+    client = new MongoClient(MONGODB_URI);
+} catch {
+    console.error("Error! Please ensure the MONGODB_URI env variable is set to a valid MongoDB connection string.");
+}
+
+const BASE_DOMAIN = process.env.BASE_DOMAIN;
+const APP_NAME = process.env.APP_NAME;
+
+handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
+app.set('view engine', 'hbs');
+
+app.set('views', './views');
+app.use('/css', express.static(path.join(__dirname, './css')));
+app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, './node_modules/bootstrap-icons/font')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/@popperjs/core/dist/cjs')));
+app.use('/js', express.static(path.join(__dirname, './node_modules/showdown/dist')));
+app.use('/img', express.static(path.join(__dirname, './img')));
+
+
+app.get('/', async (req, res) => {
+    if (client != "invalid") {
+        let assets = await getAllAssets();
+        if (process.env.DISABLE_DARKMODE) {
+            res.render('list', { "appName": APP_NAME, "assets": assets, "assetsJson": JSON.stringify(assets), "errorText": "No items exist matching that search term.", layout: "ignore-dark" });
+        } else {
+            res.render('list', { "appName": APP_NAME, "assets": assets, "assetsJson": JSON.stringify(assets), "errorText": "No items exist matching that search term." });
+        }
+    } else {
+        let assets = undefined;
+        if (process.env.DISABLE_DARKMODE) {
+            res.render('list', { "appName": APP_NAME, "assets": assets, "assetsJson": JSON.stringify(assets), "errorText": "Unable to connect to database.", layout: "ignore-dark" });
+        } else {
+            res.render('list', { "appName": APP_NAME, "assets": assets, "assetsJson": JSON.stringify(assets), "errorText": "Unable to connect to database." });
+        }
+    }
+});
+
+app.get('/:tag', async (req, res) => {
+    let asset = await getAssetInfo(req.params.tag);
+    if (process.env.DISABLE_DARKMODE) {
+        res.render('asset', { "appName": APP_NAME, "asset": asset, "domain": BASE_DOMAIN, "assetJson": JSON.stringify(asset), "id": req.params.tag, layout: "ignore-dark" });
+    } else {
+        res.render('asset', { "appName": APP_NAME, "asset": asset, "domain": BASE_DOMAIN, "assetJson": JSON.stringify(asset), "id": req.params.tag });
+    }
+});
+
+app.get('/i/:tag', async (req, res) => {
+    res.render('barcode', { "appName": APP_NAME, "id": req.params.tag, "domain": BASE_DOMAIN, layout: "ignore-dark" });
+});
+
+app.listen(3002);
+
+async function getAssetInfo(tag) {
+    try {
+        const db = client.db("asset-db");
+        const assets = db.collection("assets");
+        const query = { _id: tag };
+
+        let foundTag = await assets.findOne(query);
+        return foundTag;
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+
+async function getAllAssets() {
+    try {
+        const db = client.db("asset-db");
+        const assets = db.collection("assets");
+
+        let allAssets = await assets.find({}).toArray();
+        return allAssets;
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
